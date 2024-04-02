39c267d038750201cb29bd44370041233651bf86
Victorien GBAGUIDI <47032957+victoriengb@users.noreply.github.com>
2024-04-02 19:11:43 +0200

    Question 1 Projet

---
+++ ConsoGES.py
@@ -0,0 +1,11 @@
+class ConsoGES :
+    """ConsoGES est une classe qui représente un bien ou service produisant du Gaz à effet de serre (GES)"""
+
+    #coutGES est un réel représentant le coût en GES de l'objet
+    coutGES = 0.0
+
+    #utilité est un entier représentant le niveau d'utilité de l'objet pour celui qui le consomme
+    utilite = 0
+
+    #description est la chaîne de caractères décrivant l'objet
+    description = ""
