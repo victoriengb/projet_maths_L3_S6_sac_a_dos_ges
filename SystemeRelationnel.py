b1dbe92f41d48ca70af2402b3898e87877238fe3
Victorien GBAGUIDI <47032957+victoriengb@users.noreply.github.com>
2024-04-15 12:54:59 +0200

    Amélioration Complexité tests Propriétés

--- SystemeRelationnel.py
+++ SystemeRelationnel.py
@@ -8,11 +8,19 @@ class SystemeRelationnel :
         self.A_ensembleDesElementsDuSysteme = A_ensembleDesElementsDuSysteme
         self.R_relationBinaire = R_relationBinaire
     
+    #Permet de vérifier si un couple de sac à dos appartient à la relation binaire sans passer par l'opérateur d'appartenance in pour raison de complexité et de logique
+    #En effet, l'opérateur in utilise la méthode __eq__ de la classe SacADosGES, ce qui ne donne pas le résultat attendu et est très vorace en terme de complexité
+    def contientCoupleSacs(self, sac1, sac2) -> bool :
+        for e in self.R_relationBinaire :
+            if(e[0].estIdentique(sac1) and e[1].estIdentique(sac2)) :
+                return True
+        return False
+    
     def estReflexive(self) -> bool :
         estReflexive = True
 
         for e in self.A_ensembleDesElementsDuSysteme :
-            if not ((e, e) in self.R_relationBinaire) :
+            if not self.contientCoupleSacs(e, e) :
                 estReflexive = False
                 return estReflexive
         return estReflexive
@@ -20,22 +28,20 @@ class SystemeRelationnel :
     def estSymetrique(self) -> bool :
         estSymetrique = True
         for e in self.R_relationBinaire :
-            if not ((e[1], e[0]) in self.R_relationBinaire) :
+            if not self.contientCoupleSacs(e[1], e[0]) :
                 estSymetrique = False
                 return estSymetrique
         return estSymetrique
     
     #Générer via Google Bard car ma version avait une très mauvaise complexité
     def estTransitive(self) -> bool :
-        #Passage d'une liste de tuple à un ensemble - généré par Google Bard probablement pour des raisons de complexité dans les opération
-        relation_set = set(self.R_relationBinaire)
 
         #Parcours des tuples de la relation
         for a, b in self.R_relationBinaire:
             #Parcours des éléments de l'ensemble sur lequel porte la relation
             for c in self.A_ensembleDesElementsDuSysteme:
                 #Contrôle si la transitivité est brisée
-                if (a, b) in relation_set and (b, c) in relation_set and (a, c) not in relation_set:
+                if self.contientCoupleSacs(a, b) and self.contientCoupleSacs(b, c) and not self.contientCoupleSacs(a, c) :
                     #Si oui on retourne faux, i.e, la relation n'est pas transitive
                     return False
 
@@ -57,15 +63,12 @@ class SystemeRelationnel :
     #Générer via Google Bard car ma version avait une très mauvaise complexité
     def estNegativementTransitive(self) -> bool :
         
-        #Passage d'une liste de tuple à un ensemble - généré par Google Bard probablement pour des raisons de complexité dans les opération
-        relation_set = set(self.R_relationBinaire)
-
         #Parcours des tuples de la relation
         for a, b in self.R_relationBinaire:
             #Parcours des éléments de l'ensemble sur lequel porte la relation
             for c in self.A_ensembleDesElementsDuSysteme:
                 #Contrôle si la transitivité négative est brisée
-                if (a, b) not in relation_set and (b, c) not in relation_set and (a, c) in relation_set :
+                if not self.contientCoupleSacs(a, b) and not self.contientCoupleSacs(b, c) and self.contientCoupleSacs(a, c) :
                     #Si oui on retourne faux, i.e, la relation n'est pas négativement transitive
                     return False
         #Si non on retourne vrai, i.e, la relation est négativement transitive
@@ -86,15 +89,17 @@ class SystemeRelationnel :
     def estAntisymetrique(self) -> bool :
         estAntisymetrique = True
         for e in self.R_relationBinaire :
-            if ((e[1], e[0]) in self.R_relationBinaire) and (not e[0] == e[1]) :
+            #if ((e[1], e[0]) in self.R_relationBinaire) and (e[0] != e[1]) :
+            #if not e[0].estIdentique(e[1]) and ((e[1], e[0]) in self.R_relationBinaire) :
+            if not e[0].estIdentique(e[1]) and self.contientCoupleSacs(e[1], e[0]) :
                 estAntisymetrique = False
                 return estAntisymetrique
         return estAntisymetrique
-    
+
     def estAsymetrique(self) -> bool :
         estAsymetrique = True
         for e in self.R_relationBinaire :
-            if ((e[1], e[0]) in self.R_relationBinaire) :
+            if (self.contientCoupleSacs(e[1], e[0])) :
                 estAsymetrique = False
                 return estAsymetrique
         return estAsymetrique
@@ -103,7 +108,7 @@ class SystemeRelationnel :
         estTotale = True
         for e1 in self.A_ensembleDesElementsDuSysteme :
             for e2 in self.A_ensembleDesElementsDuSysteme :
-                if (e1 != e2 and ((e1, e2) not in self.R_relationBinaire) and ((e2, e1) not in self.R_relationBinaire)) :
+                if(not e1.estIdentique(e2) and not self.contientCoupleSacs(e1, e2) and not self.contientCoupleSacs(e2, e1)):
                     estTotale = False
                     return estTotale
         return estTotale
@@ -115,10 +120,10 @@ class SystemeRelationnel :
         distance = 0.0
         if(isinstance(systemeRelationnel2, SystemeRelationnel)) :
             for e in self.R_relationBinaire :
-                if(not e[0].__eq__(e[1]) and (e not in systemeRelationnel2.R_relationBinaire)) :
+                if(not e[0].__eq__(e[1]) and (not systemeRelationnel2.contientCoupleSacs(e))) :
                     distance += 0.5
             for e in systemeRelationnel2.R_relationBinaire :
-                if(not e[0].__eq__(e[1]) and (e not in self.R_relationBinaire)) :
+                if(not e[0].__eq__(e[1]) and (not self.contientCoupleSacs(e))) :
                     distance += 0.5
         return distance
     
